b7ea62efeaea84df3a8a5605189beea4c887a161
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-09-27 15:15:39 +0200

    Console blinking added

--- js/watchFunctions.js
+++ js/watchFunctions.js
@@ -1,4 +1,4 @@
-import { compose, curry, converge, toString, map, prop } from 'ramda';
+import { compose, curry, converge, toString, map, prop, __, equals, cond, always, modulo, concat } from 'ramda';
 import { get } from 'date-fp';
 import Either from 'sanctuary-either';
 // import IO from 'fantasy-io';
@@ -11,19 +11,26 @@ import Either from 'sanctuary-either';
 
     // const setInnerHtml = curry((id, string) => IO.of(() => document.getElementById(id).innerHTML = 8)); 
     
-    const insertInTemplate = (hours, minutes) => `
-        <span >${hours}:${minutes}</span>
+    const insertInTemplate = (hours, minutes, consoleChar) => `
+        <span>${hours}${consoleChar}${minutes}</span>
     `;
 
-    const addZero = curry((minutes) => "0" + minutes);
+    const returnConsole = cond([
+        [equals(true), always(':')],
+        [equals(false), always(' ')],
+    ]);
 
-    const isLessThanTen = curry((minutes) => minutes < 10 ? Either.Right(toString(minutes)) : Either.Left(toString(minutes)));
+    const isOdd = compose(equals(1), modulo(__, 2));
+
+    const formatConsole = compose(returnConsole, isOdd, get('milliseconds'));
+
+    const isLessThanTen = curry((minutes) => 1 < 10 ? Either.Right(toString(minutes)) : Either.Left(toString(minutes)));
     
-    const formatMinutes = compose(prop('value'), map(addZero), isLessThanTen, get('minutes'));
+    const formatMinutes = compose(prop('value'), map(concat('0')), isLessThanTen, get('minutes'));
     
     const formatHours = compose(toString, get('hours'));
 
-    const tick = compose(converge(insertInTemplate, [formatHours, formatMinutes]));
+    const tick = converge(insertInTemplate, [formatHours, formatMinutes, formatConsole]);
 
     window.onload = init();
 }());
