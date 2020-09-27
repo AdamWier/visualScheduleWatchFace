3be01dad33e909195742402f25e8ab7d8092791c
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-09-27 13:56:56 +0200

    Generates span with minutes and hours

--- js/index.js
+++ js/index.js
@@ -1,4 +1,2 @@
-import './app.js';
 import './env.js';
 import './watchFunctions.js';
-import './progressbar.js';
--- js/watchFunctions.js
+++ js/watchFunctions.js
@@ -1,135 +1,29 @@
+import { compose, curry, converge, toString, map, prop } from 'ramda';
+import { get } from 'date-fp';
+import Either from 'sanctuary-either';
+// import IO from 'fantasy-io';
+
 (function() {
-    var timerUpdateDate = 0,
-        flagConsole = false,
-        flagDigital = false,
-        arrDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-        arrMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    /**
-     * Updates the date and sets refresh callback on the next day.
-     * @private
-     * @param {number} prevDay - date of the previous day
-     */
-    function updateDate(prevDay) {
-        var datetime = tizen.time.getCurrentDateTime(),
-            nextInterval,
-            strDay = document.getElementById("str-day"),
-            strFullDate,
-            getDay = datetime.getDay(),
-            getDate = datetime.getDate(),
-            getMonth = datetime.getMonth();
-
-        // Check the update condition.
-        // if prevDate is '0', it will always update the date.
-        if (prevDay !== null) {
-            if (prevDay === getDay) {
-                /**
-                 * If the date was not changed (meaning that something went wrong),
-                 * call updateDate again after a second.
-                 */
-                nextInterval = 1000;
-            } else {
-                /**
-                 * If the day was changed,
-                 * call updateDate at the beginning of the next day.
-                 */
-                // Calculate how much time is left until the next day.
-                nextInterval =
-                    (23 - datetime.getHours()) * 60 * 60 * 1000 +
-                    (59 - datetime.getMinutes()) * 60 * 1000 +
-                    (59 - datetime.getSeconds()) * 1000 +
-                    (1000 - datetime.getMilliseconds()) +
-                    1;
-            }
-        }
-
-        if (getDate < 10) {
-            getDate = "0" + getDate;
-        }
-
-        strFullDate = arrDay[getDay] + " " + getDate + " " + arrMonth[getMonth];
-        strDay.innerHTML = strFullDate;
-
-        // If an updateDate timer already exists, clear the previous timer.
-        if (timerUpdateDate) {
-            clearTimeout(timerUpdateDate);
-        }
-
-        // Set next timeout for date update.
-        timerUpdateDate = setTimeout(function() {
-            updateDate(getDay);
-        }, nextInterval);
-    }
-
-    /**
-     * Updates the current time.
-     * @private
-     */
-    function updateTime() {
-        var strHours = document.getElementById("str-hours"),
-            strConsole = document.getElementById("str-console"),
-            strMinutes = document.getElementById("str-minutes"),
-            datetime = tizen.time.getCurrentDateTime(),
-            hour = datetime.getHours(),
-            minute = datetime.getMinutes();
-
-        strHours.innerHTML = hour;
-        strMinutes.innerHTML = minute;
-
-        if (minute < 10) {
-            strMinutes.innerHTML = "0" + minute;
-        }
-
-        // Each 0.5 second the visibility of flagConsole is changed.
-        if(flagDigital) {
-            if (flagConsole) {
-                strConsole.style.visibility = "visible";
-                flagConsole = false;
-            } else {
-                strConsole.style.visibility = "hidden";
-                flagConsole = true;
-            }
-        }
-        else {
-            strConsole.style.visibility = "visible";
-            flagConsole = false;
-        }
-        
-        if (!(state.endDateTime.getTime() > new Date().getTime())){
-        	getCalendar();
-        } else {
-        	calculateProgress();
-        }
-    }
-
-    /**
-     * Starts timer for normal digital watch mode.
-     * @private
-     */
-    function initDigitalWatch() {
-        flagDigital = true;
-        interval = setInterval(updateTime, 500);
-    }
-
-    
-    function bindEvents() {
-        // add eventListener for timetick
-        window.addEventListener("timetick", function() {
-            ambientDigitalWatch();
-        });
-    }
-
-    /**
-     * Initializes date and time.
-     * Sets to digital mode.
-     * @private
-     */
     function init() {
-        initDigitalWatch();
-        updateDate(0);
-
-        bindEvents();
+        console.log('tick', tick(tizen.time.getCurrentDateTime()));
     }
+
+    // const setInnerHtml = curry((id, string) => IO.of(() => document.getElementById(id).innerHTML = 8)); 
+    
+    const insertInTemplate = (hours, minutes) => `
+        <span >${hours}:${minutes}</span>
+    `;
+
+    const addZero = curry((minutes) => "0" + minutes);
+
+    const isLessThanTen = curry((minutes) => minutes < 10 ? Either.Right(toString(minutes)) : Either.Left(toString(minutes)));
+    
+    const formatMinutes = compose(prop('value'), map(addZero), isLessThanTen, get('minutes'));
+    
+    const formatHours = compose(toString, get('hours'));
+
+    const tick = compose(converge(insertInTemplate, [formatHours, formatMinutes]));
 
     window.onload = init();
 }());
