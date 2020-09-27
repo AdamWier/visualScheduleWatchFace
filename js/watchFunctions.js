17f9daf4893265b5a00da46d7813ba5f9ff16af7
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-09-28 00:07:13 +0200

    Get date string and refactored time string

--- js/watchFunctions.js
+++ js/watchFunctions.js
@@ -1,19 +1,31 @@
-import { compose, curry, converge, toString, map, prop, __, equals, cond, always, modulo, concat } from 'ramda';
+import { compose, curry, converge, toString, map, prop, __, equals, cond, always, modulo, concat, nth, join } from 'ramda';
 import { get } from 'date-fp';
 import Either from 'sanctuary-either';
 // import IO from 'fantasy-io';
 
 (function() {
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     function init() {
+        console.log(tizen.time.getCurrentDateTime())
         console.log('tick', tick(tizen.time.getCurrentDateTime()));
     }
 
     // const setInnerHtml = curry((id, string) => IO.of(() => document.getElementById(id).innerHTML = 8)); 
     
-    const insertInTemplate = (hours, minutes, consoleChar) => `
-        <span>${hours}${consoleChar}${minutes}</span>
+    const insertInTemplate = (time, date) => `
+        <div class="small-text">${date}</div>
+        <div class="big-text">${time}</div>
     `;
+
+    const getDate = compose(toString, get('date'));
+
+    const getMonth = compose(nth(__, months), get('month'));
+
+    const getDay = compose(nth(__, days), get('day'));
+
+    const formatDate = compose(join(' '), converge(Array.of, [getDay, getMonth, getDate]));
 
     const returnConsole = cond([
         [equals(true), always(':')],
@@ -24,13 +36,15 @@ import Either from 'sanctuary-either';
 
     const formatConsole = compose(returnConsole, isOdd, get('milliseconds'));
 
-    const isLessThanTen = curry((minutes) => 1 < 10 ? Either.Right(toString(minutes)) : Either.Left(toString(minutes)));
+    const isLessThanTen = curry((minutes) => minutes < 10 ? Either.Right(toString(minutes)) : Either.Left(toString(minutes)));
     
     const formatMinutes = compose(prop('value'), map(concat('0')), isLessThanTen, get('minutes'));
     
     const formatHours = compose(toString, get('hours'));
 
-    const tick = converge(insertInTemplate, [formatHours, formatMinutes, formatConsole]);
+    const formatTime = compose(join(''), converge(Array.of, [formatHours, formatConsole, formatMinutes]))
+
+    const tick = converge(insertInTemplate, [formatTime, formatDate]);
 
     window.onload = init();
 }());
