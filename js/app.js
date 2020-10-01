e911e69a360db0c352a0fce4d8ed57df156e5125
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-10-01 22:31:38 +0200

    Separated out emoji

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-import { compose, prop, find, propSatisfies, converge, subtract, curry, divide, multiply } from 'ramda';
+import { compose, prop, find, propSatisfies, converge, subtract, curry, divide, multiply, match, head, map, cond, isEmpty, always } from 'ramda';
 import { TEST } from './env.js';
+import Maybe from 'sanctuary-maybe';
 
 window.onload = adaptCalendarResponse(TEST);
 var state;
@@ -48,8 +49,17 @@ function adaptCalendarResponse(json){
 	const getFirstNotFullDayItem = find(propSatisfies(item => !!item.dateTime, 'end'));
 	
 	const calculatePercentage = compose(multiply(100), converge(divide, [calculateDifference, calculateRange]));
+
+	const toMaybe = cond([
+		[isEmpty, () => Maybe.Nothing],
+		[always(true), string => Maybe.Just(string)]
+	]);
 	
-	const calendar = compose(calculatePercentage, getFirstNotFullDayItem, prop('items'));
+	const emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+	
+	const getEmoji = compose(toMaybe, match(emojiRegex));
+
+	const calendar = compose(getEmoji, prop('summary'), getFirstNotFullDayItem, prop('items'));
 	
 	console.log(calendar(json));
 	
@@ -61,7 +71,6 @@ function adaptCalendarResponse(json){
 	// 	endMinutes = endMinutes.toString();
 	// }
 	
-	// var emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 	// var summary = item.summary;
 	// var emoji = summary.match(emojiRegex);
 	// var eventName;
