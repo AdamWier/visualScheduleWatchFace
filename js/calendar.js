b10e9b946284709c84c4755732d4714e21d8189b
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-11-08 20:06:26 +0100

    Avoided recreating the progress bar

--- js/calendar.js
+++ js/calendar.js
@@ -31,6 +31,7 @@ import {
 	identity,
 	lift,
 	sequence,
+	assoc,
  } from 'ramda';
 import Maybe from 'sanctuary-maybe';
 import { formatHours, formatMinutes, bothHelper, time } from './utils';
@@ -66,7 +67,7 @@ const getFirstNotFullDayItem = find(propSatisfies(item => !!item.dateTime, 'end'
 
 const liftedDivide = lift(divide);
 
-const calculatePercentage = compose(map(objOf('percentage')), map(multiply(100)), converge(liftedDivide, [calculateDifference, calculateRange]));
+const calculatePercentage = compose(map(objOf('percentage')), map(multiply(100)), chain(converge(liftedDivide, [calculateDifference, calculateRange])), prop('item'));
 
 const emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 
@@ -81,21 +82,23 @@ const toMaybe = cond([
 
 const getEmoji = compose(objOf('emoji'), toMaybe, view(lensIndex(0)), match(emojiRegex));
 
-const processEmoji = compose(resolve, removeEmoji, converge(merge, [getEmoji, objOf('description')]), prop('summary'));
+const processEmoji = compose(map(removeEmoji), map(converge(merge, [getEmoji, objOf('description')])), map(prop('summary')), prop('item'));
 
 const createMinuteProperty = compose(formatMinutes, convertToDateTime, prop('dateTime'), prop('end'));
 
 const createHourProperty = compose(formatHours, convertToDateTime, prop('dateTime'), prop('end'));
 
-const getEndString = compose(resolve, objOf('end'), join(''), converge(Array.of, [createHourProperty, () => ':', createMinuteProperty]));
+const getEndString = compose(map(objOf('end')), map(join('')), map(converge(Array.of, [createHourProperty, always(':'), createMinuteProperty])), prop('item'));
 
-const setPercentage = curry(x => attempt(() => {const circle = new tau.widget.CircleProgressBar(document.getElementById('circleprogress'), {size: 'full', thickness: 30}); circle.value(x.percentage); return x}));
+const setPercentage = curry((x) => attempt(() => {x.progressBar.value(x.percentage); return x}));
 
-const createProgressBar = compose(setPercentage);
+const initializeProgressBar = curry(x => x.isNothing ? attempt(() => new tau.widget.CircleProgressBar(document.getElementById('circleprogress'), {size: 'full', thickness: 30})) : resolve(x.value));
 
-const keepItemData = compose(resolve, objOf('original'), identity);
+const createProgressBar = compose(map(objOf('progressBar')), initializeProgressBar, prop('progressBar'))
 
-const getItemData = compose(map(mergeAll), sequence(resolve), converge(Array.of, [keepItemData, getEndString, processEmoji, calculatePercentage]));
+const keepItemData = compose(map(objOf('item')), map(identity), prop('item'));
+
+const getItemData = compose(map(mergeAll), sequence(resolve), converge(Array.of, [createProgressBar, keepItemData, getEndString, processEmoji, calculatePercentage]));
 
 const fetchFuture = curry(encaseP(fetch));
 
@@ -107,10 +110,10 @@ const toIsoString = curry(x => x.toISOString());
 
 const getApiAddress = compose(concat(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR}/events?orderBy=startTime&singleEvents=true&maxResults=10&key=${API_KEY}&timeMin=`), toIsoString);
 
-const processItem = compose(reduceRight(bothHelper, null), append(both), converge(Array.of, [chain(createProgressBar), chain(insertEventInfo)]), chain(getItemData));
+const processItem = compose(reduceRight(bothHelper, null), append(both), converge(Array.of, [chain(setPercentage), chain(insertEventInfo)]), getItemData);
 
-const callApi = compose(map(getFirstNotFullDayItem), map(prop('items')), chain(fetchJson), map(getApiAddress));
+const callApi = compose(map(getFirstNotFullDayItem), map(prop('items')), chain(fetchJson), map(getApiAddress), prop('time'));
 
-const toEither = curry(x => x.item.isNothing ? Either.Right(x.time) : Either.Left(resolve(x.item.value)));
+const toEither = curry(x => x.item.isNothing ? Either.Right(x) : Either.Left(assoc(('item'), x.item.value)(x)));
 
-export const calendar = compose(processItem, prop('value'), map(callApi), toEither);
+export const calendar = compose(processItem, prop('value'), map(converge(assoc('item'), [callApi, identity])), toEither);
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import { forkCatch, both } from 'fluture';
+import { forkCatch, both, resolve } from 'fluture';
 import { calendar } from './calendar';
-import { compose, converge, reduceRight, append, nth, merge, objOf, curry } from 'ramda';
+import { compose, converge, reduceRight, append, nth, objOf, curry, mergeAll, pick, over, lensProp } from 'ramda';
 import { tick } from './watchFunctions';
 import { bothHelper, time } from './utils';
 import Maybe from 'sanctuary-maybe';
@@ -9,12 +9,12 @@ window.onload = app;
 
 const curriedSetTimeout = curry((y, z) => setTimeout(main, y, z));
 
-const toMaybe = compose(x => x.percentage >= 100 ? Maybe.Nothing : Maybe.Just(x.original));
+const toMaybe = compose(x => x.percentage >= 100 ? Maybe.Nothing : Maybe.Just(resolve(x.item)));
 
-var onSuccess = compose(curriedSetTimeout(500), merge({time}), objOf('item'), toMaybe, nth(1), nth(1));
+var onSuccess = compose(curriedSetTimeout(500), over(lensProp('progressBar'), Maybe.Just), mergeAll, append({time}), converge(Array.of, [compose(objOf('item'), toMaybe), pick(['progressBar'])]), nth(1), nth(1));
 
 var main = compose(forkCatch(console.log)(console.log)(onSuccess), reduceRight(bothHelper, null), append(both), converge(Array.of, [calendar, tick]));
 
 function app(){
-    main({time, item: Maybe.Nothing});
+    main({time, item: Maybe.Nothing, progressBar: Maybe.Nothing});
 };
