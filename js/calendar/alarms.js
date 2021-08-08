41d498ec798f201735c8790b2f39c1a0f2294e83
AdamWier <38018787+AdamWier@users.noreply.github.com>
2021-08-08 19:25:26 +0200

    Filter out past alarms.

--- js/calendar/alarms.js
+++ js/calendar/alarms.js
@@ -1,12 +1,12 @@
-import { prop, map, curry, compose, converge, __, zipWith } from 'ramda';
+import { prop, map, curry, compose, converge, __, zipWith, filter } from 'ramda';
 import { log, getStart, getEnd, convertToDateTime } from '../utils';
 
 const addNotificationToAlarm = curry((notification, alarm) => {tizen.alarm.addAlarmNotification(alarm, notification); return alarm})
 
 const notifications = [
-	new tizen.UserNotification('SIMPLE', '50%', 
+	new tizen.UserNotification('SIMPLE', '90%', 
 		{
-			content: 'Halfway there!',
+			content: 'Time to move on',
 			actions: {
 				vibration: true,
 			},
@@ -20,14 +20,14 @@ const notifications = [
 			},
 		}
 	),
-	new tizen.UserNotification('SIMPLE', '90%', 
+	new tizen.UserNotification('SIMPLE', '50%', 
 		{
-			content: 'Time to move on',
+			content: 'Halfway there!',
 			actions: {
 				vibration: true,
 			},
 		}
-	)
+	),
 ]
 
 const createAlarm = curry(x => new tizen.AlarmAbsolute(x))
@@ -36,10 +36,12 @@ const createAlarm = curry(x => new tizen.AlarmAbsolute(x))
 
 // const disableAlarms = compose(disable, prop('id'));
 
-const calculateTime = curry((end, start, percent) => (percent * (end - start)) + start)
+const isFuture = x => x > new Date();
 
 const getTimesByPercentage = map(__, [.5, .75, .9]);
 
-const getTimeTotal = compose(getTimesByPercentage, converge(calculateTime, [getEnd, getStart]));
+const calculateTime = curry((end, start, percent) => (percent * (end - start)) + start)
 
-export default compose(map(zipWith(addNotificationToAlarm, notifications)), map(map(createAlarm)), map(map(convertToDateTime)), map(getTimeTotal), prop('item'));
+const getAlarmTimes = compose(getTimesByPercentage, converge(calculateTime, [getEnd, getStart]));
+
+export default compose(map(zipWith(addNotificationToAlarm, notifications)), map(map(createAlarm)), map(log('filtered')), map(filter(isFuture)), map(map(convertToDateTime)), map(getAlarmTimes), prop('item'));
