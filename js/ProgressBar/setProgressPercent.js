6a183611292fd708f0ccf7cc228df926bbda0378
AdamWier <38018787+AdamWier@users.noreply.github.com>
2021-08-21 14:02:07 +0200

    Separate objectification of progress bar from setting of percent

--- js/ProgressBar/setProgressPercent.js
+++ js/ProgressBar/setProgressPercent.js
@@ -1,10 +1,11 @@
-import { curry, converge } from 'ramda';
+import { curry, converge, map, compose, objOf } from 'ramda';
 import { ap, resolve } from 'fluture';
 import createProgressBar from './createProgressBar';
 import calculatePercentage from './calculatePercentage';
+import { log } from '../utils';
 
-const setPercentage = (progressBar) => percent => {progressBar.value(percent); return {progressBar}};
+const setPercentage = (progressBar) => percent => {progressBar.value(percent); return progressBar};
 
 const applyFutures = curry((functionToApply, percentage, progressBar) => (ap (percentage) (ap (progressBar) (resolve(functionToApply)))));
 
-export default converge(applyFutures(setPercentage), [calculatePercentage, createProgressBar]);
+export default compose(map(objOf('progressBar')), converge(applyFutures(setPercentage), [calculatePercentage, createProgressBar]));
