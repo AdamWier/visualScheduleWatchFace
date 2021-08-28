b06899254c7ebaf63faabc6cf28401fb7ed9488c
AdamWier <38018787+AdamWier@users.noreply.github.com>
2021-08-28 16:29:33 +0200

    Reload with tap (not very functional)

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ import { compose, converge, curry, over, lensProp, map, prop, identity, path, as
 import { tick } from './watchFunctions';
 import { time, toEither, log, toMaybe2, getEnd } from './utils';
 import Maybe from 'sanctuary-maybe';
-// import { fromEvent } from "rxjs";
+import { fromEvent } from "rxjs";
+import { debounceTime } from 'rxjs/operators';
 import getItemFromApi from './calendar/getItemFromApi';
 import setUpAlarms from './calendar/alarms';
 import processItem from './calendar/processItem';
@@ -11,13 +12,15 @@ import setProgressPercent from './ProgressBar/setProgressPercent';
 
 window.onload = app;
 
-const curriedSetTimeout = curry((y, z) => setTimeout(main, y, z));
+let timeout = 0;
+
+const curriedSetTimeout = curry((y, z) => timeout = setTimeout(main, y, z));
 
 const isNotInPast = curry((testTime) => new Date().getTime() < testTime);
 
 const isExpiredItem = compose(isNotInPast, getEnd);
 
-var onSuccess = compose(curriedSetTimeout(500), over(lensProp('alarms'), compose(resolve, objOf('alarms'))), over(lensProp('progressBar'), Maybe.Just), over(lensProp('item'), toMaybe2(isExpiredItem)), assoc('time', time), mergeAll);
+const onSuccess = compose(log('after timeout'), curriedSetTimeout(500), log('before timeout'), over(lensProp('alarms'), compose(resolve, objOf('alarms'))), over(lensProp('progressBar'), Maybe.Just), over(lensProp('item'), toMaybe2(isExpiredItem)), assoc('time', time), mergeAll);
 
 const execute = compose(fork(console.log)(onSuccess), parallel(5), values);
 
@@ -39,10 +42,14 @@ const checkItem = toEither(path(['item', 'isJust']), getItemValueFuture, identit
 
 var main = compose(execute, over(lensProp('item'), map(objOf('item'))), addTime, handleProgressBar, prop('value'), map(setupNewItem), checkItem);
 
+const DEFAULT_STATE = {time, item: Maybe.Nothing, progressBar: Maybe.Nothing, alarms: []};
+
+let state = DEFAULT_STATE;
+
 function app(){
-    main({time, item: Maybe.Nothing, progressBar: Maybe.Nothing, alarms: []});
-    // fromEvent(document, "touchstart").subscribe(() => {
-    //     result();
-    //     app();
-    // })
+    main(state);
+    fromEvent(document, "touchstart").pipe(debounceTime(500)).subscribe(() => {
+        clearTimeout(timeout)
+        main(DEFAULT_STATE);
+    })
 };
