4f9eaed6994aa7379d9fb0c5a42a2105bdfc8c09
AdamWier <38018787+AdamWier@users.noreply.github.com>
2022-04-08 21:50:11 +0200

    Cache API call result to make only one call.

--- js/calendar/getItemFromApi.js
+++ js/calendar/getItemFromApi.js
@@ -8,7 +8,8 @@ import {
  } from 'ramda';
 import { fetchJson } from '../utils';
 import { ADDRESS } from '../env';
+import { cache } from 'fluture';
 
 const getFirstNotFullDayItem = find(propSatisfies(item => !!item.dateTime, 'end'));
 
-export default compose(map(getFirstNotFullDayItem), fetchJson, always(ADDRESS));
+export default compose(map(getFirstNotFullDayItem), cache, fetchJson, always(ADDRESS));
