d9624d454a543c01057c913353d3eff487a410c5
AdamWier <38018787+AdamWier@users.noreply.github.com>
2022-09-17 00:55:50 +0200

    Bundle progress bar js in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/index.js',
+  entry: ['./js/index.js', './libs/tau/wearable/js/tau'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, ''),
