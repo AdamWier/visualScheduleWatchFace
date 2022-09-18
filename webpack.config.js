f0274e2c8c873dd1a1167d6f48cf5c7aad74d50e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2022-09-19 00:43:05 +0200

    Include css in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./js/index.js', './libs/tau/wearable/js/tau'],
+  entry: ['./index.js', './libs/tau/wearable/js/tau'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, ''),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      util: require.resolve("util/")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ]
 };
