64fc9cc15cdb6460a4ab27f63a2f49d93dc31255
AdamWier <38018787+AdamWier@users.noreply.github.com>
2023-02-22 18:50:51 +0100

    Use parallel

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import DEFAULT_STATE from './DEFAULT_STATE';
 import main, {timeout} from './main';
 import getItemFromApi from './calendar/getItemFromApi';
 import setUpAlarms from './calendar/alarms';
-import { attempt, fork, forkCatch, pap, resolve } from 'fluture';
+import { attempt, fork, forkCatch, pap, parallel, resolve } from 'fluture';
 import processItem from './calendar/processItem';
 import calculatePercentage from './ProgressBar/calculatePercentage';
 import { time, log } from './utils';
@@ -33,12 +33,13 @@ function clearOutState(){
 
 function app(){
     // main(state);
-    fork(x => console.log(x))(x => console.log(x))(compose(setUpAlarms, getItemFromApi)())
-    fork(x => console.log(x))(x => console.log(x))(compose(processItem, getItemFromApi)())
+    const alarmFuture = (compose(setUpAlarms, getItemFromApi)())
+    const insertFuture = (compose(processItem, getItemFromApi)())
     const calculatePercentageFromItem = compose(calculatePercentage, getItemFromApi)()
     const buildBar = resolve(new tau.widget.CircleProgressBar(document.getElementById('circleprogress'), {size: 'full', thickness: 30}))
-    fork(x => console.log(x))(x => console.log(x))(pap(buildBar)(pap(calculatePercentageFromItem)(resolve(setPercentage))))
-    fork(x => console.log(x))(x => console.log(x))(compose(tick, always(time))())
+    const barFuture = (pap(buildBar)(pap(calculatePercentageFromItem)(resolve(setPercentage))))
+    const timeFuture = (compose(tick, always(time))())
+    fork(console.log)(console.log)(parallel(4)([alarmFuture, insertFuture, barFuture, timeFuture, getItemFromApi()]))
 
     // fromEvent(document, "touchstart").pipe(debounceTime(60)).subscribe(compose(handleAccordingToFingers, getFingerNumber));
 };
