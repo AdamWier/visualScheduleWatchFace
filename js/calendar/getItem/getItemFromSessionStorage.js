7061c10fb0fab5ceffb33c4b274d931596149588
AdamWier <38018787+AdamWier@users.noreply.github.com>
2023-04-06 20:30:51 +0200

    Get current item based off end time (broken, needs futurized function)

--- js/calendar/getItem/getItemFromSessionStorage.js
+++ js/calendar/getItem/getItemFromSessionStorage.js
@@ -1,9 +1,20 @@
 import { attempt } from "fluture";
-import { curry, compose, chain, propSatisfies, map, find, prop } from "ramda";
-import { log, toMaybe } from "../../utils";
+import { curry, compose, map, find, prop, where, has, filter, gte, lt, invoker, } from "ramda";
+import { convertToDateTime, log, toMaybe } from "../../utils";
 
-const getFirstNotFullDayItem = find(propSatisfies(item => !!item.dateTime, 'end'));
+const callGetTime = invoker(0, 'getTime');
+
+const isBeforeNow = compose(lt(new Date().getTime()), callGetTime, convertToDateTime, prop('dateTime'));
+
+const getCurrentItem = find(where({
+    end: isBeforeNow,
+}));
+
+const getFirstNotFullDayItem = filter(where({
+    start: has('dateTime'),
+    end: has('dateTime')
+}));
 
 const getFromSessionStorage = curry(x => attempt(() => sessionStorage.getItem(x)));
 
-export default compose(map(prop('value')), map(map(getFirstNotFullDayItem)), map(map(JSON.parse)), map(toMaybe), getFromSessionStorage);
+export default compose(map(prop('value')), map(map(getCurrentItem)), map(map(getFirstNotFullDayItem)), map(map(JSON.parse)), map(toMaybe), getFromSessionStorage);
